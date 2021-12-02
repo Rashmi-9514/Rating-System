d25ef2e976717580789ad0644d7697ba86e7ac4a
Rashmi <78590807+Rashmi-9514@users.noreply.github.com>
2021-12-02 10:27:08 +0530

    Add files via upload

---
+++ 5-star-emoji-rating/README.markdown
@@ -0,0 +1,5 @@
+# 5 star emoji rating
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/rashmi-9514/pen/OJxVLeg](https://codepen.io/rashmi-9514/pen/OJxVLeg).
+
+5 star emoji rating system!
---
+++ 5-star-emoji-rating/dist/script.js
@@ -0,0 +1,7 @@
+// Simply swap out emojis from http://getemoji.com/
+
+// Working on a working rating system
+// $("span").click(function(){
+// 	$(this).addClass('selected');
+//   console.log(this);
+// });
